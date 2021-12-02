d57fae42575738da04948df27d75f62cd7baad30
egijasp <egija.srpukule@gmail.com>
2021-12-02 17:26:04 +0200

    Task 1.2 edit nestrádá ká vajag

--- src/pages/ToDo.tsx
+++ src/pages/ToDo.tsx
@@ -1,6 +1,12 @@
-import { useEffect, useRef, useState } from 'react';
+import {
+  ChangeEvent, FormEvent, useEffect, useRef, useState,
+} from 'react';
+import '../styles/Todo.scss';
+import { FaRegEdit, FaPlus, FaTimes } from 'react-icons/fa';
+import Form from '../components/Form';
 
 type Task = {
+  id: number,
     task: string,
     completed: boolean,
 }
@@ -9,25 +15,25 @@ const ToDo = () => {
   const [inputValue, setInputValue] = useState('');
   const [taskList, setTaskList] = useState<Task[]>([]);
   const [completedTask, setCompletedTask] = useState(false);
-  const input = useRef<HTMLInputElement>(null);
+  const [edit, setEdit] = useState(false);
+  const [editedTask, setEditedTask] = useState('');
 
-  useEffect(() => {
-    input.current?.focus();
-  }, []);
-
-  const submitHandler = () => {
-    if (taskList) {
-      setTaskList([...taskList, { task: inputValue, completed: false }]);
+  const addTask = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!inputValue) {
+      return;
     }
+    const newTaskList = { id: Math.random(), task: inputValue, completed: false };
+    setTaskList([...taskList, newTaskList]);
     setInputValue('');
   };
 
-  const removeHandler = (index: number) => {
+  const removeTask = (index: number) => {
     const filteredList = taskList.filter((_, i) => i !== index);
     setTaskList(filteredList);
   };
 
-  const checkHandler = (index: number) => {
+  const checkedTask = (index: number) => {
     const cloneTodoList = [...taskList];
     cloneTodoList[index].completed = !cloneTodoList[index].completed;
     setTaskList(cloneTodoList);
@@ -40,53 +46,110 @@ const ToDo = () => {
     return true;
   });
 
+  const editedTaskList = (index: number) => {
+    const updatedTaskList = [...taskList].map((task, i) => {
+      if (index === i) {
+        return { ...task, task: editedTask };
+      }
+      return task;
+    });
+    setTaskList(updatedTaskList);
+  };
+
   return (
-    <div>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        submitHandler();
-      }}
-      >
-        <label htmlFor="task">
-          Add new task
-          <input
-            name="task"
-            type="text"
-            ref={input}
-            value={inputValue}
-            placeholder="New task"
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-        </label>
-        <button type="submit">ADD</button>
-      </form>
-      <button
-        type="button"
-        onClick={() => setCompletedTask(!completedTask)}
-      >
-        Completed tasks
-      </button>
-      <div className="todo__cards">
-        {completedTasks.map(({ task, completed }, index) => (
-          <div>
+    <div className="todo">
+      <Form
+        onSubmit={addTask}
+        value={inputValue}
+        placeholder="New task"
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)}
+      />
+      <div className="todo__change">
+        <button
+          className="btn btn-todo btn-color"
+          type="button"
+          onClick={() => {
+            setTaskList([]);
+          }}
+        >
+          DELETE ALL
+        </button>
+        <button
+          className="btn btn-todo btn-color"
+          type="button"
+          onClick={() => setCompletedTask(!completedTask)}
+        >
+          COMPLETED
+        </button>
+      </div>
+      <div className="todo__list">
+        {completedTasks.map(({ task, completed, id }, index) => (
+          <div key={id} className="todo__task-wrapper">
             <input
               type="checkbox"
               checked={completed}
               onChange={() => {
-                checkHandler(index);
+                checkedTask(index);
               }}
             />
-            {task}
-            <button onClick={() => {
-              removeHandler(index);
-            }}
+            {edit ? (
+              <div className="todo__task edit">
+                <input
+                  className="input__edit"
+                  type="text"
+                  value={task}
+                  onChange={(e) => {
+                    setEditedTask(e.target.value);
+                    editedTaskList(index);
+                  }}
+                />
+                <select className="input__edit">
+                  <option value="today">Today</option>
+                  <option>This Week</option>
+                  <option>This Month</option>
+                </select>
+                <button
+                  onClick={() => {
+                    setEdit(!edit);
+                  }}
+                  type="button"
+                  className="btn btn-color edit"
+                >
+                  CANCEL
+                </button>
+                <button
+                  className="btn btn-color edit"
+                  onClick={() => {
+                    editedTaskList(index);
+                  }}
+                >
+                  SAVE
+                </button>
+              </div>
+            ) : (
+              <span className="todo__task">
+                {task}
+              </span>
+            )}
+            <button
+              className="btn"
+              onClick={() => {
+                setEdit(!edit);
+              }}
             >
-              &times;
+              <FaRegEdit />
+            </button>
+            <button
+              className="btn"
+              onClick={() => {
+                removeTask(index);
+              }}
+            >
+              <FaTimes />
             </button>
           </div>
         ))}
       </div>
-
     </div>
   );
 };
