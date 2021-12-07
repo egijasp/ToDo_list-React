faa4f7436d86a0ce9399473a48b6f191a995b2f1
egijasp <egija.srpukule@gmail.com>
2021-12-07 20:06:03 +0200

    update edit and localStorage

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
